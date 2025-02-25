6a4bd0f833b88a1ba6c40e8473032b1e2f7cdd4a
thanhdnh <myhoangthanh@yahoo.com>
2025-02-25 08:35:10 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -1,37 +1,45 @@
-﻿public class Course{
+﻿public class Course
+{
     private string id;
     private string title;
     private double weight;
     private double point;
-    public Course(string id, string title, double weight, double point){
+    public Course(string id, string title, double weight, double point)
+    {
         this.id = id;
         this.title = title;
         this.weight = weight;
         this.point = point;
     }
 
-  public double Point { get => point;}
+    public double Point { get => point; }
 
-  public override string ToString(){
+    public override string ToString()
+    {
         return $"{title}<{Point}, {weight}>";
     }
-    public static Course operator +(Course a, Course b){
-        return new Course("", "", 1, 
-            a.Point*a.weight + b.Point*b.weight);
+    public static Course operator +(Course a, Course b)
+    {
+        return new Course("", "", 1,
+            a.Point * a.weight + b.Point * b.weight);
     }
     /*public static double operator +(Course a, Course b){
         return a.point*a.weight + b.point*b.weight;
     }*/
+    public static Course operator /(Course a, Course b){
+        return new Course("", "", a.weight + b.weight, 
+            (a.Point * a.weight + 
+                b.Point * b.weight) / (a.weight + b.weight));
+    }
 }
-public class Program
-{
-    public static void Main(string[] args)
-    {
+public class Program{
+    public static void Main(string[] args){
         Console.Clear();
         Course toan = new Course("SE01", "Toan", 2, 8);
-        Course ly = new Course("SE01", "Ly", 1, 7);
-        Course hoa = new Course("SE01", "Hoa", 1, 9);
+        Course ly = new Course("SE02", "Ly", 1, 7);
+        Course hoa = new Course("SE03", "Hoa", 1, 9);
         double sum = (toan + ly + hoa).Point;
+        double gpa = (toan/ly/hoa).Point;//(2*8+1*7+1*9)/4
     }
 }
 /*
